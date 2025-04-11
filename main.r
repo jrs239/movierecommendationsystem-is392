fcc03353687a27ec4839815279eb100310be3388
Benjamin Shuster <bentzishuster613@gmail.com>
2025-04-10 21:41:32 -0400

    push data and basic code

--- main.r
+++ main.r
@@ -1,2 +1,5 @@
-val <- "Hello IS392!!!"
-print(val)
+file_path <- "netflix_data.csv"
+data<- read.csv(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+View(data)
+col_names<-colnames(data)
+print(col_names)
