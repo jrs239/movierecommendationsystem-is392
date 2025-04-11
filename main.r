2f5ea05b64347712beecca1feeb025f9baec0f9e
Jorge Sanchez <jrs239@njit.edu>
2025-04-10 21:51:44 -0400

    test

--- main.r
+++ main.r
@@ -3,3 +3,7 @@ data<- read.csv(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 View(data)
 col_names<-colnames(data)
 print(col_names)
+
+name <= readline("Enter your name: ")
+age <= readline("Enter your age:")
+movie <= readline("Enter your favorite movie genre:")
