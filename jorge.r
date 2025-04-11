c59107460b9dfb7b219641d9d8dc8ee8545c74e9
Jorge Sanchez <jrs239@njit.edu>
2025-04-10 22:12:53 -0400

    first merge steps 1 & 2

--- jorge.r
+++ jorge.r
@@ -3,10 +3,37 @@ library(tidyr)
 library(reshape2)
 library(recommenderlab)
 
+# Check for missing values and na values
+missing_values <- sapply(data, function(x) sum(is.na(x)))
+na_values <- sapply(data, function(x) sum(x == "NA"))
+print(missing_values)
+
+# check for duplicates (if there are any... IDK)
+duplicates <- duplicated(data)
+print(paste("Number of duplicate rows:", sum(duplicates))) # print the number of duplicate rows
+#paste 
+
+# Get a list the well rated movies
+# Assuming the dataset has a 'rating' column and a 'title' column
+popular_movies <- data[order(data$rating, decreasing = TRUE), ]
+popular_movies <- popular_movies[1:50, ] # Select top 50 movies
+# Print the titles of the popular movies
+print(popular_movies$title)
+
+
+ten_random_movies <- data[sample(nrow(data), 10), ] # Select 10 random movies
+print(ten_random_movies$title) # Print the titles of the random movies
+# convert the titles to a list
+list_of_movies <- as.list(ten_random_movies$title)
+
+#user input below
+
 #name <= readline("Enter your name: ")
 #age <= readline("Enter your age:")
 #movie <= readline("Enter your favorite movie genre:")
 
+
+#simulated user input
 user.ratings <- data.frame(
     movies = c()
 
