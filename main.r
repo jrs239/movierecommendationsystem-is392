a5f44f68a38645f7692c1eeba92bd1e38834baf3
Benjamin Shuster <bentzishuster613@gmail.com>
2025-04-10 21:49:29 -0400

    check for dupes and NAs

--- main.r
+++ main.r
@@ -1,5 +1,13 @@
 file_path <- "netflix_data.csv"
 data<- read.csv(file_path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
-View(data)
 col_names<-colnames(data)
-print(col_names)
+
+# Check for missing values and na values
+missing_values <- sapply(data, function(x) sum(is.na(x)))
+na_values <- sapply(data, function(x) sum(x == "NA"))
+print(missing_values)
+
+# check for duplicates (if there are any... IDK)
+duplicates <- duplicated(data)
+print(paste("Number of duplicate rows:", sum(duplicates))) # print the number of duplicate rows
+#paste 
